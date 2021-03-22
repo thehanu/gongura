c739b6e6ad7a5f9979bd95ae9aff5029a60821bc
manuuser <buchiki@outlook.com>
2021-03-22 16:16:49 -0700

    Added reverse logic

--- Scripts/reverse.ps1
+++ Scripts/reverse.ps1
@@ -1,5 +1,5 @@
 # open names.txt into stream_reader
-$stream_reader = New-Object System.IO.StreamReader("C:\Users\buchi\OneDrive\Documents\Names.txt")
+$stream_reader = New-Object System.IO.StreamReader("C:\Users\buchi\OneDrive\Documents\Names.Big.txt")
 
 # read next line and assign to current_line
 $current_line =$stream_reader.ReadLine()
@@ -10,6 +10,23 @@ while($current_line -ne $null)
     # write current_line
     $current_line
 
+    #reverse line to nothing
+    $reversed_line = ""
+
+    #figure out length of current line
+    $current_line.length
+
+    #  reverse line from 8
+    for($i=$current_line.length ; $i -ge 0; $i-- )
+    {
+        $reversed_line = $reversed_line + $current_line[$i] 
+        # $reversed_line
+    }
+    
+    # reversed line
+    $reversed_line
+
     # read next line and assign to current_line
     $current_line =$stream_reader.ReadLine()
 }
+$stream_reader.Close()
